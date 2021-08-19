f91a4976a28984d4d43c90a53a6b5e8db8de70a0
Bryan  <bryanvillamil@iMac-de-BrYaN.local>
2021-08-18 19:05:32 -0500

    changs in fix the typescript

--- src/components/molecules/toggleModeDark/index.tsx
+++ src/components/molecules/toggleModeDark/index.tsx
@@ -3,12 +3,23 @@ import React from 'react'
 // import { ReactComponent as MoonIcon } from 'icons/moon.svg';
 // import { ReactComponent as SunIcon } from 'icons/sun.svg';
 
-const Toggle = ({ theme, toggleTheme }) => {
-  const isLight = theme === 'light';
+// { theme, toggleTheme }
+// const Toggle = () => {
+//   const isLight = theme === 'light';
+//   return (
+//     <button onClick={toggleTheme} >
+//       {/* <SunIcon />
+//       <MoonIcon /> */}
+//       toggle
+//     </button>
+//   );
+// };
+
+// export default Toggle;
+
+const Toggle = () => {
   return (
-    <button onClick={toggleTheme} >
-      {/* <SunIcon />
-      <MoonIcon /> */}
+    <button>
       toggle
     </button>
   );
